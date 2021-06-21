58efc634c77c09fb1d85b891241548d2a4ef62f9
Conte Berengario <conteberengario@gmail.com>
2021-06-21 09:44:37 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Sono un Grande!'));
+app.get('/', (req, res) => res.send('Lo ero prima, ma lo sono ancora...un Grande!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
